bf2711304920e6b72ff7e6fce81cec61c17f6170
Patrick Mueller <patrick-m-96@web.de>
2025-01-02 18:21:20 -0800

    - Added existing raytrace script and a simple example script. - GPU is not fully implemented yet.

---
+++ examples/simple_ex.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+examples.simple_ex
+==================
+
+Simple example script of a raytracing scene.
+"""
+
+import optrace.raytrace as r
+
+r.test_trace_loop_gpu(1000)
